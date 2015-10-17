1d5faec1a0518a74e435dc410928bd35f7439465
rougehero <stannajl@miamioh.edu>
2015-10-17 13:18:46 -0400

    Create controller.cpp

---
+++ controller.cpp
@@ -0,0 +1,15 @@
+#include "Fractal.h"
+
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    //YOUR CODE STARTS HERE
+	cout << "hello, World" << endl;
+    //YOUR CODE ENDS HERE
+
+    return 0;
+}
